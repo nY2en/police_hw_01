07753cd880a3967cef8b2ddd38d93f298d78822c
Nikita Chebanenko <cheba@MacBook-Pro-Nikita.local>
2023-10-04 10:43:03 +0300

    try/catch

--- utils/fetchPokemons.js
+++ utils/fetchPokemons.js
@@ -1,10 +1,14 @@
 const BASE_URL = "https://pokeapi.co/api/v2";
 
 async function fetchPokemons() {
-  const response = await fetch(`${BASE_URL}/pokemon`);
-  const data = await response.json();
+  try {
+    const response = await fetch(`${BASE_URL}/pokemon`);
+    const data = await response.json();
 
-  return data.results;
+    return data.results;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 export default fetchPokemons;
